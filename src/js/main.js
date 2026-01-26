ce274918c3e2047e969f1ad99ac8a5d9c954bfe6
schmitz-eline <eline.schmitz@student.hepl.be>
2026-01-26 20:02:19 +0100

    Hero carousel layout and animation

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,22 @@ import {settings} from "./settings";
 const reproduction = {
     init() {
         this.getHtmlElements();
+        this.initData();
         this.manageMobileNavMenu();
+        this.addHeroCarouselNav()
     },
 
     getHtmlElements() {
         this.mobileLinks = document.querySelectorAll(settings.mobileMenuLinkSelector);
         this.menuToggle = document.getElementById(settings.menuToggleId);
         this.submenuCheckboxes = document.querySelectorAll(settings.submenuCheckboxSelector);
+        this.heroSlides = document.querySelectorAll(settings.heroSlideSelector);
+        this.heroPreviousSlide = document.querySelector(settings.heroPreviousSelector);
+        this.heroNextSlide = document.querySelector(settings.heroNextSelector);
+    },
+
+    initData() {
+        this.heroSlideIndex = 0;
     },
 
     manageMobileNavMenu() {
@@ -30,6 +39,24 @@ const reproduction = {
                 }
             });
         });
+    },
+    addHeroCarouselNav() {
+        this.heroPreviousSlide.addEventListener('click', () => {
+            this.heroSlideIndex = (this.heroSlideIndex - 1 + this.heroSlides.length) % this.heroSlides.length;
+            this.displayHeroSlide(this.heroSlideIndex);
+        });
+
+        this.heroNextSlide.addEventListener('click', () => {
+            this.heroSlideIndex = (this.heroSlideIndex + 1) % this.heroSlides.length;
+            this.displayHeroSlide(this.heroSlideIndex);
+        });
+    },
+
+    displayHeroSlide(index) {
+        this.heroSlides.forEach((slide) => {
+            slide.classList.remove(settings.isActiveClass);
+        });
+        this.heroSlides[index].classList.add(settings.isActiveClass);
     },
 };
 
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,5 +1,9 @@
 export const settings = {
     mobileMenuLinkSelector: '.mobile-menu a',
     menuToggleId: 'menu-toggle',
-    submenuCheckboxSelector: '.submenu-checkbox'
+    submenuCheckboxSelector: '.submenu-checkbox',
+    heroSlideSelector: '.hero__slide',
+    heroPreviousSelector: '.hero__nav--previous',
+    heroNextSelector: '.hero__nav--next',
+    isActiveClass: 'is-active'
 }
