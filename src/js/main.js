991e8694dd24d002b37cafa57d0dd5c436c64477
schmitz-eline <eline.schmitz@student.hepl.be>
2026-01-27 19:17:51 +0100

    Ticker layout and animation

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,9 @@ const reproduction = {
         this.getHtmlElements();
         this.initData();
         this.manageMobileNavMenu();
-        this.addHeroCarouselNav()
+        this.addHeroCarouselNav();
+        this.manageTickerScroll();
+        this.addTickerNav();
     },
 
     getHtmlElements() {
@@ -19,6 +21,7 @@ const reproduction = {
 
     initData() {
         this.heroSlideIndex = 0;
+        this.offset = 0;
     },
 
     manageMobileNavMenu() {
@@ -58,6 +61,44 @@ const reproduction = {
         });
         this.heroSlides[index].classList.add(settings.isActiveClass);
     },
+
+    manageTickerScroll() {
+        document.addEventListener('DOMContentLoaded', () => {
+            const track = document.querySelector(settings.tickerTrackSelector);
+            const items = [...track.children];
+            this.fillTicker(track, items);
+            window.addEventListener('resize', this.fillTicker);
+        });
+    },
+
+    fillTicker(track, items) {
+        const viewportWidth = window.innerWidth;
+        while (track.scrollWidth < viewportWidth * 2) {
+            items.forEach((item) => {
+                track.appendChild(item.cloneNode(true));
+            });
+        }
+    },
+
+    addTickerNav() {
+        document.addEventListener('DOMContentLoaded', () => {
+            const previous = document.querySelector(settings.tickerPreviousSelector);
+            const next = document.querySelector(settings.tickerNextSelector);
+            next.addEventListener('click', () => this.moveViewport(-1));
+            previous.addEventListener('click', () => this.moveViewport(1));
+        });
+    },
+
+    moveViewport(direction) {
+        const inner = document.querySelector(settings.tickerInnerSelector);
+        const item = document.querySelector(settings.tickerItemSelector);
+        const itemWidth = item.offsetWidth + parseFloat(getComputedStyle(item).columnGap);
+        this.offset += direction * itemWidth;
+        if ((this.offset > window.innerWidth / 4) || (this.offset < -window.innerWidth / 4)) {
+            this.offset = 0;
+        }
+        inner.style.transform = `translateX(${this.offset}px)`;
+    }
 };
 
 reproduction.init();
--- src/js/settings.js
+++ src/js/settings.js
@@ -5,5 +5,10 @@ export const settings = {
     heroSlideSelector: '.hero__slide',
     heroPreviousSelector: '.hero__nav--previous',
     heroNextSelector: '.hero__nav--next',
-    isActiveClass: 'is-active'
+    isActiveClass: 'is-active',
+    tickerTrackSelector: '.ticker__track',
+    tickerPreviousSelector: '.ticker__nav--previous',
+    tickerNextSelector: '.ticker__nav--next',
+    tickerItemSelector: '.ticker__item',
+    tickerInnerSelector: '.ticker__inner',
 }
